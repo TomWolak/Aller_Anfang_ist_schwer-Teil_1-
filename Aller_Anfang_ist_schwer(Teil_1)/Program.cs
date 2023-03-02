103ba29043ea05440ba9950a46f2850c3c35bad8
TomWolak <tomek@wolak.be>
2023-03-02 08:40:20 +0100

    Initial commit

---
+++ Aller_Anfang_ist_schwer(Teil_1)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aller_Anfang_ist_schwer_Teil_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
